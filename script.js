c6d7f3ef450512e8205e1bc561027ea25d3ee1d6
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 22:34:54 -0400

    backup

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
   console.log("hello world")
 })
+
+var ul = $('ul')
+
+var liTags = $('li')
+console.log(liTags)
+
+var red = $('#red')
+console.log(red)
+
+var liClassBlack = $('.black')
+console.log(liClassBlack)
+
+var firstBlackLi = $('.black').eq(0)
+console.log(firstBlackLi)
+
+var lastBlackLi = $('.black:last-of-type')
+console.log(lastBlackLi)
